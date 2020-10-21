d3aa5d479b144f22b857bb1491ccd3572097147b
genmakerlab@gmail.com <genmakerlab@gmail.com>
2020-10-20 19:33:52 -0500

    in progress

---
+++ Area_of_Rec/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Area_of_Rec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of rectangle.");
+            string length = Console.ReadLine();
+
+            Console.WriteLine("Enter the width.");
+            string width = Console.ReadLine();
+
+            int convertedLength = int.Parse(length);
+            int convertedwidth = int.Parse(width);
+            int area = convertedLength * convertedwidth;
+
+            Console.WriteLine("The area of your rectangle is " + area);
+
+            Console.WriteLine("this line is here to pause my console display");
+
+        }
+    }
+}
---
+++ Miles_Driven_Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Miles_Driven_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles per gallon does your car get?");
+            double milesPerGallon = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many miles did you drive?");
+            double milesDriven = Convert.ToInt32(Console.ReadLine());
+
+            double gasUsed = milesDriven / milesPerGallon;
+            Console.WriteLine("You have used " + gasUsed + " gallons of gas.");
+
+            Console.WriteLine("this line is here to pause my console display");
+
+
+        }
+    }
+}
