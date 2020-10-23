9687ab7207abd8039f92a0aff059fdc67da0c98f
genforsythe <genmakerlab@gmail.com>
2020-10-22 20:23:30 -0500

    Finished Chapter 2 exercise. Not pretty but works

--- string_crawler/Program.cs
+++ string_crawler/Program.cs
@@ -4,10 +4,11 @@ namespace string_crawler
 {
     class Program
     {
-        //private static object converted_pargraph;
 
         static void Main(string[] args)
         {
+
+
             string paragraph = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
 
             string converted_paragraph = paragraph.ToLower();
@@ -16,36 +17,40 @@ namespace string_crawler
             string userInput = Console.ReadLine();
 
             string converted_userInput = userInput.ToLower();
+            //string modified_str = paragraph;
 
             if (converted_paragraph.Contains(converted_userInput))
             {
-                Console.WriteLine("Your word " + userInput + " is in the paragraph!")
-
                 // var set to count number of user input occurences
                 int at = 0;
                 int end = converted_paragraph.Length;
-                int start = 1;
-                int count = 0;
+                int start = 0;
+                int count;
+                
+                Console.WriteLine("Your word " + userInput + " is in the paragraph at index ");
 
                 // while loop to print out all occurences of a user input word
                 while ((start <= end) && (at > -1))
                 {
                     count = end - start;
                     at = converted_paragraph.IndexOf(converted_userInput, start, count);
+                    if (at >= 0) {
+                    string modified_str = paragraph.Remove(at,userInput.Length);
+                    Console.WriteLine(modified_str);
+                    }
                     if (at == -1) break;
-                    Console.WriteLine("{0} ", at);
+                    Console.WriteLine(at);
                     start = at + 1;
-
+                    
                 }
-             
-            }
-            else
+            }else
             {
                 Console.WriteLine("Your word " + userInput + " was not in the paragraph.");
             }
-        }
-        
-    }
+            
 
+        }
+
+    }
 }
 
